8f5ea18c4a84a0a7880feff067a7abe9e4e65c54
Qin87 <qinjiang19871018@gmail.com>
2025-03-28 19:27:57 +0000

    tel run in batch

---
+++ nest_tel.h
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+net_values="  GCN "
+layer_values=" 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 100 200 300 400 500 600 1000 2000  "
+#layer_values="   1  15 16 17 18 19 20 30 40 50 60 70 "   1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+
+# 'citeseer/' 'cora_ml/'  'telegram/'   'dgl/pubmed'  'WikiCS/'     --net="$net"  'WikipediaNetwork/chameleon'
+Direct_dataset=(   'telegram/'     )
+Direct_dataset_filename=$(echo $Direct_dataset | sed 's/\//_/g')
+generate_timestamp() {
+  date +"%d%H%Ms%S"
+}
+timestamp=$(generate_timestamp)
+
+# Iterate over each dataset   --net="$net"    --layer="$layer"
+for Didataset in "${Direct_dataset[@]}"; do
+    for layer in $layer_values; do
+        logfile="outforlayer${layer}.log"
+        exec > "$logfile" 2>&1  # Redirect stdout and stderr to log file
+        # for imba_value  in $imbal; do
+        for net in $net_values; do
+            log_output="${Didataset//\//_}_${timestamp}_${net}_layer${layer}q${q_value}.log"
+
+            # Run the Python script with parameters and log output
+            python3 main.py  --net="$net"   --to_reverse_edge=0 --use_best_hyperparams=1   --Dataset='telegram/'  --add_selfloop=1  --to_undirected=0
+            --Dataset="$Didataset" > "$log_output"
+             2>&1
+            wait $pid
+          # done
+        done
+        done
+done
+
+
+
