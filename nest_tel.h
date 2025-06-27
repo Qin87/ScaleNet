0f4b60742aaefe0cc0eb258c14e0e05ef5f02f41
Qin <qj2004@hw.ac.uk>
2025-06-27 14:19:13 +0100

    4

--- nest_tel.h
+++ nest_tel.h
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 net_values="  GCN "
-layer_values=" 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20  "
-# layer_values="1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 30 40 50 60 70 "   1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+layer_values=" 7 8 9 10 11 12 13 14 15 16 17 18 19 20  "
 
-# 'citeseer/' 'cora_ml/'  'telegram/'   'dgl/pubmed'  'WikiCS/'     --net="$net"  'WikipediaNetwork/chameleon'
 Direct_dataset=(   'WikipediaNetwork/squirrel'     )
 Direct_dataset_filename=$(echo $Direct_dataset | sed 's/\//_/g')
 generate_timestamp() {
@@ -12,7 +10,6 @@ generate_timestamp() {
 }
 timestamp=$(generate_timestamp)
 
-# Iterate over each dataset   --net="$net"    --layer="$layer"
 for Didataset in "${Direct_dataset[@]}"; do
     for layer in $layer_values; do
         logfile="outforlayer${layer}.log"
@@ -22,7 +19,7 @@ for Didataset in "${Direct_dataset[@]}"; do
             log_output="${Didataset//\//_}_${timestamp}_${net}_layer${layer}q${q_value}.log"
 
             # Run the Python script with parameters and log output
-            python3 main.py  --net="$net"  --layer="$layer" --to_reverse_edge=1 --use_best_hyperparams=1  --add_selfloop=1  --to_undirected=0 \
+ python3 main.py  --net="$net"   --GPU=1  --layer="$layer" --to_reverse_edge=0  --use_best_hyperparams=1  --add_selfloop=0  --to_undirected=1 \
             --Dataset="$Didataset" > "$log_output"
              2>&1
             wait $pid
