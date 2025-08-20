073d970c308c7bc2abccf95c0678b801b7d22d88
Qin87 <qinjiang19871018@gmail.com>
2025-08-20 20:52:31 +0100

    struct.h

--- batch_sh/struct.h
+++ batch_sh/struct.h
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-net_values="link linkx  "
-layer_values="  6 7 2 3 4 5 1 "
+net_values="link linkx link_concat linkx_git "
+layer_values="2 "
 imbal="100  "
-
+Binary="0 1 "
 
 # 'citeseer/' 'cora_ml/'  'telegram/'   'dgl/pubmed'  'WikiCS/'  'WikipediaNetwork/chameleon' 'WikipediaNetwork/squirrel'
-Direct_dataset=(  'WikipediaNetwork/squirrel' )
+Direct_dataset=(  'fb100/penn94' )
 generate_timestamp() {
   date +"%d%H%Ms%S"
 }
@@ -18,15 +18,19 @@ for Didataset in "${Direct_dataset[@]}"; do
         logfile="outforlayer${layer}.log"
         exec > "$logfile" 2>&1  # Redirect stdout and stderr to log file
         # for imba_value  in $imbal; do
+         for binall1 in $Binary; do
+         for binUndir in $Binary; do
+         # for binReverse in $Binary; do
         for net in $net_values; do
             log_output="${Didataset//\//_}_${timestamp}_${net}_layer${layer}.log"
 
             # Run the Python script with parameters and log output
-            python3 main.py    --net="$net"   --layer="$layer" \
+            python3 main.py    --net="$net"   --layer="$layer"   --all1="$binall1"   --to_undirected="$binUndir" \
             --Dataset="$Didataset" > "$log_output"
              2>&1
             wait $pid
-          # done
+          done
+          done
         done
         done
 done
