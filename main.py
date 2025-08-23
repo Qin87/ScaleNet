15d0aef76159516074bc294feda74fc54db9ce2e
Qin87 <qinjiang19871018@gmail.com>
2025-08-23 08:28:39 +0100

    pokec dataset done!

--- main.py
+++ main.py
@@ -451,46 +451,6 @@ try:
                     except:
                         data_test_mask = data_test_maskOrigin.clone()
 
-            # all_list = [
-            #     ("no_in_nodes", no_in_nodes),
-            #     ("in_homophilic_nodes", in_homophilic_nodes),
-            #     ("in_heterophilic_nodes", in_heterophilic_nodes),
-            #     ("no_out_nodes", no_out_nodes),
-            #     ("out_homophilic_nodes", out_homophilic_nodes),
-            #     ("out_heterophilic_nodes", out_heterophilic_nodes)
-            # ]
-
-            # noIn_noOut_nodes = list(set(no_in_nodes) & set(no_out_nodes))
-            # noIn_outHomo_nodes = list(set(no_in_nodes) & set(out_homophilic_nodes))
-            # noIn_outHetero_nodes = list(set(no_in_nodes) & set(out_heterophilic_nodes))
-            # inHomo_noOut_nodes = list(set(in_homophilic_nodes) & set(no_out_nodes))
-            # inHomo_outHomo_nodes = list(set(in_homophilic_nodes) & set(out_homophilic_nodes))
-            # inHomo_outHetero_nodes = list(set(in_homophilic_nodes) & set(out_heterophilic_nodes))
-            # inHetero_noOut_nodes = list(set(in_heterophilic_nodes) & set(no_out_nodes))
-            # inHetero_outHomo_nodes = list(set(in_heterophilic_nodes) & set(out_homophilic_nodes))
-            # inHetero_outHetero_nodes = list(set(in_heterophilic_nodes) & set(out_heterophilic_nodes))
-
-            # New combined list with intersections
-            # combined_intersection_list = [
-            #     ("noIn_noOut_nodes", noIn_noOut_nodes),
-            #     ("noIn_outHomo_nodes", noIn_outHomo_nodes),
-            #     ("noIn_outHetero_nodes", noIn_outHetero_nodes),
-            #     ("inHomo_noOut_nodes", inHomo_noOut_nodes),
-            #     ("inHomo_outHomo_nodes", inHomo_outHomo_nodes),
-            #     ("inHomo_outHetero_nodes", inHomo_outHetero_nodes),
-            #     ("inHetero_noOut_nodes", inHetero_noOut_nodes),
-            #     ("inHetero_outHomo_nodes", inHetero_outHomo_nodes),
-            #     ("inHetero_outHetero_nodes", inHetero_outHetero_nodes)
-            #     ,('all nodes', list(range(data_x.shape[0])))
-            # ]
-
-            # for name, lst in combined_intersection_list:
-            #     if len(lst) == 0:
-            #         print(f"{name}:No Node")
-            #         continue
-            #     mask = create_mask(lst, data_x.shape[0]).to(device)
-            #     train_temp, val_temp, test_temp = mask & data_train_mask, mask & data_val_mask, mask & data_test_mask
-            #     # print(f"{name}: Train={train_temp.sum().item()}, Val={val_temp.sum().item()}, Test={test_temp.sum().item()}")
 
             n_data0 = []  # num of train in each class
             for i in range(n_cls):
@@ -597,9 +557,6 @@ try:
                     # print('test_f1 CountNotImproved reset to 0 in epoch', epoch, file=log_file)
                     # Store the calculated metrics in variables instead of printing
 
-                    # for name, lst in combined_intersection_list:
-                    #     metrics_temp = calculate_metrics(logits, data_test_mask, data_y, lst, edges)
-                    #     metrics_list.append((name, metrics_temp))
 
                 else:
                     CountNotImproved += 1
@@ -609,8 +566,6 @@ try:
                     # end_time = time.time()
                     print('epoch: {:3d}, val_loss:{:2f}, test_acc: {:.2f}, bacc: {:.2f}, tmp_test_acc: {:.2f}, f1: {:.2f}'.format(epoch, val_loss, test_acc * 100, test_bacc * 100, tmp_test_acc*100,
                                                                                                                               test_f1 * 100))
-                    # print(end_time - start_time, file=log_file)
-                    # print(end_time - start_time)
                     print('epoch: {:3d}, val_loss:{:2f}, test_acc: {:.2f}, bacc: {:.2f}, tmp_test_f1: {:.2f}, f1: {:.2f}'.format(epoch, val_loss, test_acc * 100, test_bacc * 100, tmp_test_f1*100,
                                                                                                                              test_f1 * 100),file=log_file)
                 end_epoch = epoch
