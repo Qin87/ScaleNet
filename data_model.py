d091b5bd79e1a5cfc1b6a8b2ce2832621d5b506d
Qin87 <qinjiang19871018@gmail.com>
2025-08-23 07:04:39 +0100

    linkxadd done!

--- data_model.py
+++ data_model.py
@@ -54,7 +54,7 @@ def CreatModel(args, num_features, n_cls, data_x,device, num_edges=None):
     elif args.net.lower() == 'linkxadd':
         model = LINK_Add(num_features, args.hid_dim, n_cls, args.layer, args.num_node,
             inner_activation=args.inner_activation, inner_dropout=args.inner_dropout, dropout=args.dropout, init_layers_A=args.link_init_layers_A, init_layers_X=args.link_init_layers_X).to(device)
-    elif args.net.lower() == 'linkx':
+    elif args.net.lower() == 'linkx':  # linkx is opposite aggregation of linkxgit
         model = LINKX(num_nodes=args.num_node, in_channels= num_features, hidden_channels=args.hid_dim, out_channels=n_cls, num_layers=args.layer,
             num_edge_layers=args.link_init_layers_A, num_node_layers=args.link_init_layers_X,
                        dropout=args.dropout).to(device)
