3a14b5fbc6ca5c3d88e00350fb20b5383b78d87d
Qin87 <qinjiang19871018@gmail.com>
2025-08-25 22:13:11 +0100

    scale.h

--- batch_sh/scale.h
+++ batch_sh/scale.h
@@ -3,6 +3,7 @@
 net_values="LargeScaleNet  ScaleNet  "
 layer_values=" 3 "
 Dir="0 0.5 1  -1"
+Patiences=(10 100  )
 
 # 'citeseer_npz/' 'cora_ml/'  'telegram/' 'dgl/pubmed' 'WikiCS/'
 #  'WikipediaNetwork/squirrel'  'WikipediaNetwork/chameleon'  'directed-roman-empire/'
@@ -14,6 +15,7 @@ timestamp=$(generate_timestamp)
 
 # Iterate over each dataset
 for Didataset in "${Direct_dataset[@]}"; do
+      for patience in "${Patiences[@]}"; do
     for layer in $layer_values; do
         logfile="outforlayer${layer}.log"
         exec > "$logfile" 2>&1  # Redirect stdout and stderr to log file
@@ -24,13 +26,14 @@ for Didataset in "${Direct_dataset[@]}"; do
             log_output="${Didataset//\//_}_${timestamp}_A${a}_alpha${dir}__${net}_layer${layer}.log"
 
             # Run the Python script with parameters and log output
-python3 main.py   --alphaDir="$alphadir"  --betaDir="$betadir"    --gamaDir="$gamadir"  --num_split=1  --add_selfloop=1
---patience=100 --use_best_hyperparams=1 \
+python3 main.py   --alphaDir="$alphadir"  --betaDir="$betadir"    --gamaDir="$gamadir"  --num_split=1  --add_selfloop=1  \
+--patience="$patience" --use_best_hyperparams=1 \
  --net="$net"  --layer="$layer"   --Dataset="$Didataset" >  \
 "$log_output"
              2>&1
             wait $pid
         done
+       done
        done
        done
        done
