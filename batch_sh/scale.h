4957efe20cbb293a57f0b86c7080afd3237907f4
Qin87 <qinjiang19871018@gmail.com>
2025-08-26 22:07:56 +0100

    seed for best

--- batch_sh/scale.h
+++ batch_sh/scale.h
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-net_values="LargeScaleNet"
+net_values="ScaleNet  "
 layer_values=" 3 "
 Dir="0 0.5 1  -1"
-Patiences=(10 40  )
+Patiences=(10 100  )
+aDir=" 1  -1"
+bDir="0.5 1  -1"
 
-# 'citeseer_npz/' 'cora_ml/'  'telegram/' 'dgl/pubmed' 'WikiCS/'
+
+while pgrep -x python3 > /dev/null; do
+  echo "Waiting for all python3 processes to finish..."
+  sleep 10
+done
+
+# 'citeseer_npz/' 'cora_ml/'  'telegram/' 'dgl/pubmed' 'WikiCS/'  'arxiv-year/' 'fb100/penn94'
 #  'WikipediaNetwork/squirrel'  'WikipediaNetwork/chameleon'  'directed-roman-empire/'
-Direct_dataset=('directed-roman-empire/' )
+Direct_dataset=('directed-roman-empire/'  )
 generate_timestamp() {
   date +"%d%H%Ms%S"
 }
@@ -19,14 +27,14 @@ for Didataset in "${Direct_dataset[@]}"; do
     for layer in $layer_values; do
         logfile="outforlayer${layer}.log"
         exec > "$logfile" 2>&1  # Redirect stdout and stderr to log file
-        for alphadir in $Dir; do
-        for betadir in $Dir; do
+        for alphadir in $aDir; do
+        for betadir in $bDir; do
         for gamadir in $Dir; do
         for net in $net_values; do
             log_output="${Didataset//\//_}_${timestamp}_A${a}_alpha${dir}__${net}_layer${layer}.log"
 
             # Run the Python script with parameters and log output
-python3 main.py   --alphaDir="$alphadir"  --betaDir="$betadir"    --gamaDir="$gamadir"  --num_split=1  --add_selfloop=0  \
+python3 main.py   --alphaDir="$alphadir"  --betaDir="$betadir"   --BN_model=0   --gamaDir="$gamadir"  --num_split=1  --add_selfloop=1  \
 --patience="$patience" --use_best_hyperparams=1 \
  --net="$net"  --layer="$layer"   --Dataset="$Didataset" >  \
 "$log_output"
