353a44fc074757bd5875d70f8d9b8741f62265e6
Qin87 <qinjiang19871018@gmail.com>
2025-08-29 11:22:08 +0100

    arxiv-year

--- batch_sh/scale.h
+++ batch_sh/scale.h
@@ -2,8 +2,8 @@
 
 net_values="LargeScaleNet"
 layer_values="5 6 7 8"
-Dir="-1"
-Patiences=(80 200  )
+Dir="0.5 1 0 -1"
+Patiences=(80 200 400 )
 
 while pgrep -x python3 > /dev/null; do
   echo "Waiting for all python3 processes to finish..."
@@ -12,7 +12,7 @@ done
 
 # 'citeseer_npz/' 'cora_ml/'  'telegram/' 'dgl/pubmed' 'WikiCS/'
 #  'WikipediaNetwork/squirrel'  'WikipediaNetwork/chameleon'  'directed-roman-empire/'
-Direct_dataset=('directed-roman-empire/' )
+Direct_dataset=('arxiv-year/' )
 generate_timestamp() {
   date +"%d%H%Ms%S"
 }
@@ -25,13 +25,13 @@ for Didataset in "${Direct_dataset[@]}"; do
         logfile="outforlayer${layer}.log"
         exec > "$logfile" 2>&1  # Redirect stdout and stderr to log file
    #        for alphadir in $Dir; do
-        for betadir in $Dir; do
-      #  for gamadir in $Dir; do
+       for betadir in $Dir; do
+        for gamadir in $Dir; do
         for net in $net_values; do
             log_output="${Didataset//\//_}_${timestamp}_A${a}_alpha${dir}__${net}_layer${layer}.log"
 
             # Run the Python script with parameters and log output
-python3 main.py   --alphaDir=0.5  --betaDir="$betadir"    --gamaDir=-1  --num_split=1  --add_selfloop=0  \
+python3 main.py   --alphaDir=0.5  --betaDir="$betadir"    --gamaDir="$gamadir"  --num_split=1  --add_selfloop=0  \
 --NotImproved="$patience"  --use_best_hyperparams=1 \
  --net="$net"  --layer="$layer"   --Dataset="$Didataset" >  \
 "$log_output"
