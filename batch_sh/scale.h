4f88a2870843183eb1af158a6a32043bc16fa335
Qin87 <qinjiang19871018@gmail.com>
2025-09-03 23:18:29 +0100

    drouput

--- batch_sh/scale.h
+++ batch_sh/scale.h
@@ -2,9 +2,9 @@
 
 net_values="LargeScaleNet"
 layer_values="8"
-bDir="64"
-Dir="0.01 0.05 0.1 "
-Patiences=(200 400 )
+bDir="1 2 3 4"
+Dir="0  0.5 "
+Patiences=(0 1)
 
 while pgrep -x python3 > /dev/null; do
   echo "Waiting for all python3 processes to finish..."
@@ -31,8 +31,8 @@ for Didataset in "${Direct_dataset[@]}"; do
             log_output="${Didataset//\//_}_${timestamp}_A${a}_alpha${dir}__${net}_layer${layer}.log"
 
             # Run the Python script with parameters and log output
-python3 main_lit.py   --alphaDir=0.5  --betaDir=0    --gamaDir=0  --num_split=1  --add_selfloop=0  \
---NotImproved="$patience"  --hid_dim="$betadir" --lr="$gamadir" --use_best_hyperparams=1  --all1=0  --weight_penalty='None'\
+python3 main_lit.py  --seed="$betadir" --alphaDir=0.5  --betaDir=0    --gamaDir=0  --num_split=1  --add_selfloop=0  \
+--NotImproved="$patience"  --hid_dim="$betadir" --dropout="$gamadir" --use_best_hyperparams=1  --all1="$patience"  --weight_penalty='None'\
  --net="$net"  --layer="$layer"   --Dataset="$Didataset" >  \
 "$log_output"
              2>&1
