094c4b4c25d5a621d9664036783632f5066f9c4c
Qin87 <qinjiang19871018@gmail.com>
2025-09-05 00:15:16 +0100

    LargeScaleNet has all1

--- batch_sh/axive.h
+++ batch_sh/axive.h
@@ -1,20 +1,19 @@
 #!/bin/bash
 
 net_values="LargeScaleNet"
-layer_values="3 4 5 6"
-bDir="1 2 3 4"
+layer_values="6"
 Dir="0  0.5 1 -1"
-Patiences=(80  200  400)
-Normalize_vals=(0  1)   # 1
+Patiences=(400)
+Normalize_vals=(0)   # 1
 
 while pgrep -x python3 > /dev/null; do
   echo "Waiting for all python3 processes to finish..."
-  sleep 1800
+  sleep 180
 done
 
 # 'citeseer_npz/' 'cora_ml/'  'telegram/' 'dgl/pubmed' 'WikiCS/'
 #  'WikipediaNetwork/squirrel'  'WikipediaNetwork/chameleon'  'directed-roman-empire/'
-Direct_dataset=('ogbn-arxiv/' )
+Direct_dataset=('directed-roman-empire/' )
 generate_timestamp() {
   date +"%d%H%Ms%S"
 }
@@ -38,7 +37,7 @@ for Didataset in "${Direct_dataset[@]}"; do
             # Run the Python script with parameters and log output
 python3 main_lit.py  --seed=111 --alphaDir="$alphadir"  --betaDir="$betadir"   --gamaDir="$gamadir"  --num_split=1   \
 --NotImproved="$patience"  --use_best_hyperparams=1     --weight_penalty='None'\
-  --normalize="$normalize"   --zero_order="$zero"  --BN_model="$BN"  --add_selfloop="$sloop"  --dropout=0.0  \
+  --normalize="$normalize"   --zero_order="$zero"  --BN_model="$BN"  --add_selfloop="$sloop"  --dropout=0.0  --all1=1 \
  --net="$net"  --layer="$layer"   --Dataset="$Didataset" >  \
 "$log_output"
              2>&1
@@ -52,4 +51,5 @@ python3 main_lit.py  --seed=111 --alphaDir="$alphadir"  --betaDir="$betadir"   -
 done
        done
        done
+done
 done
