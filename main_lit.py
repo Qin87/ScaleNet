4dc0705d7ea76ee5e9ff331aebd2f59261946970
Qin87 <qinjiang19871018@gmail.com>
2025-09-07 00:52:00 +0100

    use ddp

--- main_lit.py
+++ main_lit.py
@@ -14,7 +14,7 @@ from pytorch_lightning.callbacks import EarlyStopping, ModelSummary, ModelCheckp
 from torch.utils.data import DataLoader
 from ogb.nodeproppred import Evaluator
 
-from .utils.args import parse_args
+from utils.args import parse_args
 from data.data_utils import  set_device, seed_everything
 from utils.data_model import CreatModel, load_dataset, name_file, free_space, rename_log
 from nets.lit_model import FullBatchGraphDataset, LightingFullBatchModelWrapper
