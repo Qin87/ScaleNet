a3ab246ad5a636dd2b65763cf653ea4637e0e3e4
Qin87 <qinjiang19871018@gmail.com>
2025-09-07 02:39:51 +0100

    test_mask

--- utils/args.py
+++ utils/args.py
@@ -5,7 +5,6 @@ def parse_args():
     parser.add_argument("--monitor", type=str, help="optimiser monitor: val_acc(acc), val_loss(loss)", default="val_acc")
 
     parser.add_argument("--use_best_hyperparams", type=int, default=1, help="whether use parameters in best_hyperparameters.yml")
-    parser.add_argument('--multiple_GPU',  type=int, default=1, help='add selfloop in before model')
     parser.add_argument('--GPU', type=int, default=0, help='GPU device number')
     parser.add_argument('--CPU', action='store_true', help='use CPU even has GPU')
     parser.add_argument("--BN_model", type=int, help="whether use layer normalization in model:0/1", default=0)
