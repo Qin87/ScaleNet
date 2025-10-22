ab3879eaada8b1bfaf0dce19a55b0cb91a76dbae
Qin87 <qinjiang19871018@gmail.com>
2025-10-22 11:57:55 +0100

    degfea2

--- utils/data_model.py
+++ utils/data_model.py
@@ -215,8 +215,10 @@ def CreatModel(args, num_features, n_cls, data_x,device, num_edges=None):
 def get_name(args, IsDirectedGraph):
     dataset_to_print = args.Dataset.replace('/', '_')
 
-    if args.all1:
+    if args.all1 and not args.degfea:
         dataset_to_print += 'all1'
+    elif args.degfea:
+        dataset_to_print += 'degfea' + str(args.degfea)
     if not IsDirectedGraph:
         dataset_to_print = dataset_to_print + 'Undire'
     else:
