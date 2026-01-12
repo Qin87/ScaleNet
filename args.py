1e7131e846f44e1bddb5cb0bcefac16ad1da9de3
Qin87 <qinjiang19871018@gmail.com>
2026-01-12 07:31:51 +0000

    UAT debug

--- args.py
+++ args.py
@@ -33,7 +33,7 @@ def parse_args():
     parser.add_argument('--MakeImbalance', '-imbal', action='store_true', help='if convert graph to undirecteds')  # TODO change before git
     parser.add_argument('--imb_ratio', type=float, default=100, help='imbalance ratio')
 
-    parser.add_argument('--net', type=str, default='GAT', help='ScaleNet, Dir-GNN '
+    parser.add_argument('--net', type=str, default='UAT', help='ScaleNet, Dir-GNN '
                      'Mag, Sig, QuaNet, '
                     'GCN, GAT, SAGE, Cheb, APPNP, GPRGNN, pgnn, mlp, sgc'
                     'DiGib, DiGub,DiGi3, DiGi4 (1iG, RiG replace DiG)'
