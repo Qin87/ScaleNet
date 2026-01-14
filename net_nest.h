ec350fcd752b11ab0b93c6efb9531c354033f037
Qin87 <qinjiang19871018@gmail.com>
2026-01-14 11:08:36 +0000

    GCN debug

--- net_nest.h
+++ net_nest.h
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 net_values="GAT  "
-layer_values="2   "
-incinorm=" dir row sym 0 "   # dir   row
+layer_values="2 4  "
+incinorm="softmax"   # dir   row
 lr0="0.005 0.01"
 
 while pgrep -x python3 >/dev/null; do
     sleep 300
 done
 
-# # PubMed, Coauthor-physics, Coauthor-CS, Amazon-Computers, Amazon-Photo
+# # PubMed, Coauthor-physics, , Amazon-Computers, Amazon-Photo, 'Amazon-Computers' 'Coauthor-CS'
 # 'citeseer/' 'cora_ml/'  'telegram/'   'dgl/pubmed'  'WikiCS/'   'WikiCS/' 'WikipediaNetwork/squirrel'
 # 'WikipediaNetwork/chameleon'   'telegram/'   'dgl/pubmed'  'citeseer/'   #"$inci"
-Direct_dataset=(     'Amazon-Photo'      )
+Direct_dataset=(     'telegram/'   )
 Direct_dataset_filename=$(echo $Direct_dataset | sed 's/\//_/g')
 generate_timestamp() {
   date +"%d%H%Ms%S"
@@ -30,7 +30,7 @@ for Didataset in "${Direct_dataset[@]}"; do
             log_output="${Didataset//\//_}_${timestamp}_${net}_layer${layer}.log"
 
             python3 main.py   --net="$net"  --layer="$layer"  --use_best_hyperparams=1 --num_split=10   --inci_norm="$inci"  \
-            --originGAT=0 --r20_per_class=1  --to_undirected=1 --lr="$lr" --BN_model=0  \
+            --originGAT=1 --r20_per_class=1  --to_undirected=1 --lr="$lr" --BN_model=0  \
             --Dataset="$Didataset" > "$log_output"
              2>&1
             wait $pid
