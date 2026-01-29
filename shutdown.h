2f96783fd74f8a45a0525bd4e19c51adfe60f00b
Qin87 <qinjiang19871018@gmail.com>
2026-01-29 18:21:27 +0000

    remove more

--- shutdown.h
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Wait until no python / python3 process is running for this user
-while pgrep -u "$USER" -f "python" > /dev/null; do
-    sleep 2000
-done
-
-# Shutdown the system
-sudo shutdown -h now
