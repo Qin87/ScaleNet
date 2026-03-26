eb8c98ebd85ae36d2455d0de0de1ad28794b3468
Qin87 <qinjiang19871018@gmail.com>
2026-03-26 16:27:12 +0000

    args in DiGCN

--- args.py
+++ args.py
@@ -2,7 +2,7 @@ import argparse
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--use_best_hyperparams", type=int, help="use parameters in best_hyperparameters.yml", default=1)
+    parser.add_argument("--use_best_hyperparams", type=int, help="use parameters in best_hyperparameters.yml", default=0)
     parser.add_argument('--GPU', type=int, default=0, help='device')
     parser.add_argument('--r20_per_class', type=int, default=0, help='1 train split is random 20 nodes_per_class')
 
@@ -11,7 +11,7 @@ def parse_args():
     parser.add_argument('--posweight', type=str, default='abs', help='positive attention: abs, 2, e for exp,or 0 for None')
     parser.add_argument('--CPU', action='store_true', help='use CPU even has GPU')
     parser.add_argument("--BN_model", type=int, help="whether use layer normalization in model:0/1", default=0)
-    parser.add_argument("--nonlinear", type=int, help="whether use activation(relu) in ScaleNet model:0/1", default=1)
+    parser.add_argument("--nonlinear", type=int, help="whether use activation(relu) in ScaleNet model:0/1", default=0)
     parser.add_argument("--First_self_loop", type=str, choices=["add", "remove",  0], default=0, help="Whether to add self-loops to the graph")
     parser.add_argument("--rm_gen_sloop", type=str, choices=["remove", 0], default=0, help="Whether to remove generated self-loops to the graph")
 
@@ -20,12 +20,12 @@ def parse_args():
 
     # for DirGNN
     parser.add_argument("--normalize", type=int, help="whether use layer normalization in ScaleNet, model:0/1", default=1)
-    parser.add_argument("--inci_norm", type=str, choices=["dir", "sym", 'row', '0', 'softmax'], default='0')
+    parser.add_argument("--inci_norm", type=str, choices=["dir", "sym", 'row', '0', 'softmax'], default='dir')
     parser.add_argument('--num_split', type=int, default=10, help='num of run in spite of many splits')
 
-    parser.add_argument('--net', type=str, default='UiGib', help='GAT, RAT, UAT, RiGib, AiGib, UiGib')
+    parser.add_argument('--net', type=str, default='GAT', help='GAT, RAT, UAT, RiGib, AiGib, UiGib')
     parser.add_argument('--seed', type=int, default=1, help='random seed')
-    parser.add_argument('--Dataset', type=str, default='telegram/', help='telegram/ , cora_ml/, citeseer/,  WikiCS/'
+    parser.add_argument('--Dataset', type=str, default='snap-patents/', help='telegram/ , cora_ml/, citeseer/,  WikiCS/'
                 'PubMed, Coauthor-physics, Coauthor-CS, Amazon-Computers, Amazon-Photo')
     parser.add_argument('--dropout', type=float, default=0.5, help='dropout prob')
     parser.add_argument('--layer', type=int, default=3, help='number of layers (2 or 3), default: 2')
@@ -46,7 +46,7 @@ def parse_args():
 
     parser.add_argument('--W_degree', type=int, default=5, help='using in-degree_0, out-degree_1, full-degree_2 for DiG edge-weight, 3 is random[1,100], 4 is random[0.1,1], 5 is random[0.0001, '
                                                                 '10000], 50 is abs(sin(random5))')
-    parser.add_argument('--to_undirected', '-tud', type=int, default=0, help='if convert graph to undirected')
+    parser.add_argument('--to_undirected', '-tud', type=int, default=1, help='if convert graph to undirected')
 
 
     args = parser.parse_args()
