3c078623aba138fc593caa9fe7832d6f813e7af4
Qin87 <qinjiang19871018@gmail.com>
2026-07-18 21:08:15 +0100

    no edge weight

--- nets/gcn.py
+++ nets/gcn.py
@@ -205,13 +205,13 @@ class GraphSAGEXBatNorm(nn.Module):
         if self.layer > 2:
             for iter_layer in self.convx:
                 x = F.dropout(x, p=self.dropout_p, training=self.training)
-                x = iter_layer(x, adj,edge_weight)
+                x = iter_layer(x, adj)
                 if self.BN:
                     x = self.batch_norm3(x)
                 x = F.relu(x)
 
         x = F.dropout(x, p=self.dropout_p, training=self.training)
-        x = self.conv2(x, adj,edge_weight)
+        x = self.conv2(x, adj)
         if self.BN:
             x = self.batch_norm2(x)
 
